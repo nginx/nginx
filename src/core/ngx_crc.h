0ab9e437145620ea9e44c03173c9e60d9b29475b
Igor Sysoev <igor@sysoev.ru>
2006-10-13 15:09:25 +0000

    style fix

--- src/core/ngx_crc.h
+++ src/core/ngx_crc.h
@@ -10,15 +10,16 @@
 
 /* 32-bit crc16 */
 
-static ngx_inline uint32_t ngx_crc(char *data, size_t len)
+static ngx_inline uint32_t
+ngx_crc(char *data, size_t len)
 {
     uint32_t  sum;
 
     for (sum = 0; len; len--) {
 
         /*
-         * gcc 2.95.2 x86 and icc 7.1.006 compile that operator
-         *                                into the single "rol" opcode.
+         * gcc 2.95.2 x86 and icc 7.1.006 compile
+         * that operator into the single "rol" opcode,
          * msvc 6.0sp2 compiles it into four opcodes.
          */
         sum = sum >> 1 | sum << 31;
