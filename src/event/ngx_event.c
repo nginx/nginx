17e0e1a554c6bc5b7b03522857a58a4befd7284b
Igor Sysoev <igor@sysoev.ru>
2006-11-23 20:22:24 +0000

    fix segfault

--- src/event/ngx_event.c
+++ src/event/ngx_event.c
@@ -584,7 +584,7 @@ ngx_event_process_init(ngx_cycle_t *cycle)
     ccf = (ngx_core_conf_t *) ngx_get_conf(cycle->conf_ctx, ngx_core_module);
     ecf = ngx_event_get_conf(cycle->conf_ctx, ngx_event_core_module);
 
-    if (ccf->worker_processes > 1 && ecf->accept_mutex) {
+    if (ccf->master && ccf->worker_processes > 1 && ecf->accept_mutex) {
         ngx_use_accept_mutex = 1;
         ngx_accept_mutex_held = 0;
         ngx_accept_mutex_delay = ecf->accept_mutex_delay;
