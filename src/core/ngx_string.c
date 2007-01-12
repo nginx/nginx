37cc165064fd681ef453c59e28a9170d3338979a
Igor Sysoev <igor@sysoev.ru>
2007-01-12 20:15:59 +0000

    ngx_strn2cmp()

--- src/core/ngx_string.c
+++ src/core/ngx_string.c
@@ -487,6 +487,31 @@ ngx_rstrncasecmp(u_char *s1, u_char *s2, size_t n)
 
 
 ngx_int_t
+ngx_strn2cmp(u_char *s1, u_char *s2, size_t n1, size_t n2)
+{
+    size_t     n;
+    ngx_int_t  m, z;
+
+    if (n1 <= n2) {
+        n = n1;
+        z = -1;
+
+    } else {
+        n = n2;
+        z = 1;
+    }
+
+    m = ngx_strncmp(s1, s2, n);
+
+    if (m || n1 == n2) {
+        return m;
+    }
+
+    return z;
+}
+
+
+ngx_int_t
 ngx_atoi(u_char *line, size_t n)
 {
     ngx_int_t  value;
