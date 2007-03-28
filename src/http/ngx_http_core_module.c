4ca98f65a5b2e753bdbb235d4609fbf3e0e323d6
Igor Sysoev <igor@sysoev.ru>
2007-03-28 14:38:33 +0000

    style fix

--- src/http/ngx_http_core_module.c
+++ src/http/ngx_http_core_module.c
@@ -874,7 +874,8 @@ ngx_http_update_location_config(ngx_http_request_t *r)
     }
 
     if (r == r->main) {
-            r->connection->log->file = clcf->err_log->file;
+        r->connection->log->file = clcf->err_log->file;
+
         if (!(r->connection->log->log_level & NGX_LOG_DEBUG_CONNECTION)) {
             r->connection->log->log_level = clcf->err_log->log_level;
         }
