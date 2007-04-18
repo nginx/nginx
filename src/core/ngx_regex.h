eb21763fa13fb746987e39a571abbc8a510ae33a
Igor Sysoev <igor@sysoev.ru>
2007-04-18 19:43:22 +0000

    style fix

--- src/core/ngx_regex.h
+++ src/core/ngx_regex.h
@@ -22,10 +22,10 @@ typedef pcre  ngx_regex_t;
 
 void ngx_regex_init(void);
 ngx_regex_t *ngx_regex_compile(ngx_str_t *pattern, ngx_int_t options,
-                               ngx_pool_t *pool, ngx_str_t *err);
+    ngx_pool_t *pool, ngx_str_t *err);
 ngx_int_t ngx_regex_capture_count(ngx_regex_t *re);
-ngx_int_t ngx_regex_exec(ngx_regex_t *re, ngx_str_t *s,
-                         int *captures, ngx_int_t size);
+ngx_int_t ngx_regex_exec(ngx_regex_t *re, ngx_str_t *s, int *captures,
+    ngx_int_t size);
 
 #define ngx_regex_exec_n           "pcre_exec()"
 #define ngx_regex_capture_count_n  "pcre_fullinfo()"
