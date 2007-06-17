def1e7439fe14d343368e372b611891f976e4c5e
Igor Sysoev <igor@sysoev.ru>
2007-06-17 09:29:15 +0000

    if subrequest response was buffered in file, then subrequest was finalized with 0 code, ngx_http_writer() was not set, and response part in file was lost

--- src/http/ngx_http_upstream.c
+++ src/http/ngx_http_upstream.c
@@ -2157,8 +2157,17 @@ ngx_http_upstream_finalize_request(ngx_http_request_t *r,
 
     r->connection->log->action = "sending to client";
 
-    if (rc == 0 && r == r->main && !r->post_action) {
-        rc = ngx_http_send_special(r, NGX_HTTP_LAST);
+    if (rc == 0) {
+        if (r == r->main) {
+            if (!r->post_action) {
+                rc = ngx_http_send_special(r, NGX_HTTP_LAST);
+            }
+
+        } else {
+            if (r->out) {
+                rc = NGX_AGAIN;
+            }
+        }
     }
 
     ngx_http_finalize_request(r, rc);
