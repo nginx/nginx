254353e40e73211ebcbbc9db468c2481a2b7101f
Igor Sysoev <igor@sysoev.ru>
2007-09-15 16:54:58 +0000

    the "proxy_hide_header" and "fastcgi_hide_header" directives did not hide response header lines whose name was longer than 32 characters

--- src/http/modules/ngx_http_fastcgi_module.c
+++ src/http/modules/ngx_http_fastcgi_module.c
@@ -1135,7 +1135,7 @@ ngx_http_fastcgi_process_header(ngx_http_request_t *r)
 
                 } else {
                     for (i = 0; i < h->key.len; i++) {
-                        h->lowcase_key[i] = ngx_tolower(h->lowcase_key[i]);
+                        h->lowcase_key[i] = ngx_tolower(h->key.data[i]);
                     }
                 }
 
