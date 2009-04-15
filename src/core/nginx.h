57006fbb13caaab7015ca6a3f448ff081880ecab
Igor Sysoev <igor@sysoev.ru>
2009-04-15 06:25:02 +0000

    bump version

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version       007051
-#define NGINX_VERSION      "0.7.51"
+#define nginx_version         7052
+#define NGINX_VERSION      "0.7.52"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
