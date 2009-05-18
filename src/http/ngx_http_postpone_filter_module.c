a948e1f3216cd529eefd11fb4548b3dcb16d9178
Igor Sysoev <igor@sysoev.ru>
2009-05-18 15:39:08 +0000

    r2296 merge:
    fix r2105 (r2067 in trunk)

--- src/http/ngx_http_postpone_filter_module.c
+++ src/http/ngx_http_postpone_filter_module.c
@@ -216,7 +216,7 @@ ngx_http_postpone_filter_output_postponed_request(ngx_http_request_t *r)
         r->postponed = r->postponed->next;
     }
 
-    if (r->out) {
+    if (r != r->main && r->out) {
         ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                        "http postpone filter out again \"%V?%V\"",
                        &r->uri, &r->args);
