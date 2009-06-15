nginx/nginx

commit d734aad8f2441a9781d79fc932ef86b8ee576347

inherit proxy_set_header, proxy_hide_header, and fastcgi_hide_header only if cache settings are similar
git-svn-id: svn://svn.nginx.org/nginx/trunk@2930 c3fe7df1-7212-e011-8a91-001109144009

src/http/modules/ngx_http_proxy_module.c
@@ -2342,7 +2342,9 @@ ngx_http_proxy_merge_headers(ngx_conf_t *cf, ngx_http_proxy_loc_conf_t *conf,
         conf->headers_source = prev->headers_source;
     }
 
-    if (conf->headers_set_hash.buckets) {
+    if (conf->headers_set_hash.buckets
+        && ((conf->upstream.cache == NULL) == (prev->upstream.cache == NULL)))
+    {
         return NGX_OK;
     }
 

src/http/ngx_http_upstream.c
@@ -4085,7 +4085,9 @@ ngx_http_upstream_hide_headers_hash(ngx_conf_t *cf,
     {
         conf->hide_headers_hash = prev->hide_headers_hash;
 
-        if (conf->hide_headers_hash.buckets) {
+        if (conf->hide_headers_hash.buckets
+            && ((conf->cache == NULL) == (prev->cache == NULL)))
+        {
             return NGX_OK;
         }
 

commit 0775182312de6b279bb99cc23d644382db04870a

fix building --without-http-cache, broken in r2930

src/http/modules/ngx_http_proxy_module.c
@@ -2343,7 +2343,10 @@ ngx_http_proxy_merge_headers(ngx_conf_t *cf, ngx_http_proxy_loc_conf_t *conf,
     }
 
     if (conf->headers_set_hash.buckets
-        && ((conf->upstream.cache == NULL) == (prev->upstream.cache == NULL)))
+#if (NGX_HTTP_CACHE)
+        && ((conf->upstream.cache == NULL) == (prev->upstream.cache == NULL))
+#endif
+       )
     {
         return NGX_OK;
     }

src/http/ngx_http_upstream.c
@@ -4086,7 +4086,10 @@ ngx_http_upstream_hide_headers_hash(ngx_conf_t *cf,
         conf->hide_headers_hash = prev->hide_headers_hash;
 
         if (conf->hide_headers_hash.buckets
-            && ((conf->cache == NULL) == (prev->cache == NULL)))
+#if (NGX_HTTP_CACHE)
+            && ((conf->cache == NULL) == (prev->cache == NULL))
+#endif
+           )
         {
             return NGX_OK;
         }