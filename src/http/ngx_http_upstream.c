704462ac85ec5afffb79abc874664d9bbf679ece
Igor Sysoev <igor@sysoev.ru>
2009-06-22 09:10:50 +0000

    fix building --without-http-cache, broken in r2953

--- src/http/ngx_http_upstream.c
+++ src/http/ngx_http_upstream.c
@@ -318,9 +318,13 @@ static ngx_http_variable_t  ngx_http_upstream_vars[] = {
       ngx_http_upstream_response_length_variable, 0,
       NGX_HTTP_VAR_NOHASH|NGX_HTTP_VAR_NOCACHEABLE, 0 },
 
+#if (NGX_HTTP_CACHE)
+
     { ngx_string("upstream_cache_status"), NULL,
       ngx_http_upstream_cache_status, 0,
       NGX_HTTP_VAR_NOHASH|NGX_HTTP_VAR_NOCACHEABLE, 0 },
+
+#endif
 
     { ngx_null_string, NULL, NULL, 0, 0, 0 }
 };
