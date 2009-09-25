812234e2f5b063811d9ea39affe29f60f50fefc1
Igor Sysoev <igor@sysoev.ru>
2009-09-25 11:55:33 +0000

    fix discarding body

--- src/http/ngx_http_request_body.c
+++ src/http/ngx_http_request_body.c
@@ -562,10 +562,14 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                    "http read discarded body");
 
-    do {
+    for ( ;; ) {
         if (r->headers_in.content_length_n == 0) {
             r->read_event_handler = ngx_http_block_reading;
             return NGX_OK;
+        }
+
+        if (!r->connection->read->ready) {
+            return NGX_AGAIN;
         }
 
         size = (r->headers_in.content_length_n > NGX_HTTP_DISCARD_BUFFER_SIZE) ?
@@ -588,10 +592,7 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
         }
 
         r->headers_in.content_length_n -= n;
-
-    } while (r->connection->read->ready);
-
-    return NGX_AGAIN;
+    }
 }
 
 
