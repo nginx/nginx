2495c4d50bbdaf1410808fd230ecca820c463934
Igor Sysoev <igor@sysoev.ru>
2009-10-19 14:08:09 +0000

    add lingering timeout if a response is short and a request body is being discarded, the bug was introduced in r3050

--- src/http/ngx_http_request.c
+++ src/http/ngx_http_request.c
@@ -2101,12 +2101,19 @@ ngx_http_finalize_connection(ngx_http_request_t *r)
 {
     ngx_http_core_loc_conf_t  *clcf;
 
+    clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
+
     if (r->main->count != 1) {
+
+        if (r->discard_body && r->lingering_time == 0) {
+            r->lingering_time = ngx_time()
+                                      + (time_t) (clcf->lingering_time / 1000);
+            ngx_add_timer(r->connection->read, clcf->lingering_timeout);
+        }
+
         ngx_http_close_request(r, 0);
         return;
     }
-
-    clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
 
     if (!ngx_terminate
          && !ngx_exiting
