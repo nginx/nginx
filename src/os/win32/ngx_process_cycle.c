nginx/nginx

commit e2c551bc514dddf081e8f17d4737d5773b38c611

win32 cache loader support
git-svn-id: svn://svn.nginx.org/nginx/trunk@3023 c3fe7df1-7212-e011-8a91-001109144009

src/os/win32/ngx_process_cycle.c
@@ -25,6 +25,7 @@ static void ngx_worker_process_exit(ngx_cycle_t *cycle);
 static ngx_thread_value_t __stdcall ngx_worker_thread(void *data);
 static ngx_thread_value_t __stdcall ngx_cache_manager_thread(void *data);
 static void ngx_cache_manager_process_handler(void);
+static ngx_thread_value_t __stdcall ngx_cache_loader_thread(void *data);
 
 
 ngx_uint_t     ngx_process;
@@ -670,6 +671,10 @@ ngx_worker_process_cycle(ngx_cycle_t *cycle, char *mevn)
         goto failed;
     }
 
+    if (ngx_create_thread(&cmtid, ngx_cache_loader_thread, NULL, log) != 0) {
+        goto failed;
+    }
+
     for ( ;; ) {
         ev = WaitForMultipleObjects(3, events, 0, INFINITE);
 
@@ -982,6 +987,34 @@ ngx_cache_manager_process_handler(void)
         ngx_log_debug1(NGX_LOG_DEBUG_CORE, ngx_cycle->log, 0,
                        "cache manager WaitForSingleObject: %ul", ev);
     }
+}
+
+
+static ngx_thread_value_t __stdcall
+ngx_cache_loader_thread(void *data)
+{
+    ngx_uint_t     i;
+    ngx_path_t   **path;
+    ngx_cycle_t   *cycle;
+
+    ngx_msleep(60000);
+
+    cycle = (ngx_cycle_t *) ngx_cycle;
+
+    path = cycle->pathes.elts;
+    for (i = 0; i < cycle->pathes.nelts; i++) {
+
+        if (ngx_terminate || ngx_quit) {
+            break;
+        }
+
+        if (path[i]->loader) {
+            path[i]->loader(path[i]->data);
+            ngx_time_update(0, 0);
+        }
+    }
+
+    return 0;
 }
 
 

commit 2952af08f17ee98645191c4e34a9cd84c024d30d

a cache manager thread handle was overwritten by a cache loader thread handle, this caused an exit delay, the bug had been introduced in r3023

src/os/win32/ngx_process_cycle.c
@@ -599,7 +599,7 @@ ngx_worker_process_cycle(ngx_cycle_t *cycle, char *mevn)
     HANDLE      mev, events[3];
     u_long      nev, ev;
     ngx_err_t   err;
-    ngx_tid_t   wtid, cmtid;
+    ngx_tid_t   wtid, cmtid, cltid;
     ngx_log_t  *log;
 
     log = cycle->log;
@@ -671,7 +671,7 @@ ngx_worker_process_cycle(ngx_cycle_t *cycle, char *mevn)
         goto failed;
     }
 
-    if (ngx_create_thread(&cmtid, ngx_cache_loader_thread, NULL, log) != 0) {
+    if (ngx_create_thread(&cltid, ngx_cache_loader_thread, NULL, log) != 0) {
         goto failed;
     }
 