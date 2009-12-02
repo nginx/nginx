6cfcef3cbfd37b1172bd79ccdcddab3436eda6a4
Igor Sysoev <igor@sysoev.ru>
2009-12-02 13:03:54 +0000

    remove stale comment

--- src/core/ngx_resolver.c
+++ src/core/ngx_resolver.c
@@ -89,8 +89,6 @@ static void ngx_resolver_free_locked(ngx_resolver_t *r, void *p);
 static void *ngx_resolver_dup(ngx_resolver_t *r, void *src, size_t size);
 
 
-/* STUB: ngx_peer_addr_t * */
-
 ngx_resolver_t *
 ngx_resolver_create(ngx_conf_t *cf, ngx_addr_t *addr)
 {
