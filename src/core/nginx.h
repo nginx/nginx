9a5eabc4ac28ec90537dcd27f348738a19d199e1
Igor Sysoev <igor@sysoev.ru>
2009-12-25 14:57:10 +0000

    bump version

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version         8031
-#define NGINX_VERSION      "0.8.31"
+#define nginx_version         8032
+#define NGINX_VERSION      "0.8.32"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
