185a5d172671b4b4346d801e144e055f3962d55d
Igor Sysoev <igor@sysoev.ru>
2010-04-22 13:35:30 +0000

    make $arg_ variables non-cacheable

--- src/http/ngx_http_variables.c
+++ src/http/ngx_http_variables.c
@@ -1937,6 +1937,7 @@ ngx_http_variables_init_vars(ngx_conf_t *cf)
         if (ngx_strncmp(v[i].name.data, "arg_", 4) == 0) {
             v[i].get_handler = ngx_http_variable_argument;
             v[i].data = (uintptr_t) &v[i].name;
+            v[i].flags = NGX_HTTP_VAR_NOCACHEABLE;
 
             continue;
         }
