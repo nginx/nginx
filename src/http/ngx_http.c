5644c43caf8130d511758b8f9727c4fd4e0388d4
Igor Sysoev <igor@sysoev.ru>
2010-12-12 21:13:27 +0000

    always run regex in server_name to get captures for IPv6 addresses, the same fix for IPv4 addresses has been made in r2584

--- src/http/ngx_http.c
+++ src/http/ngx_http.c
@@ -1871,7 +1871,11 @@ ngx_http_add_addrs6(ngx_conf_t *cf, ngx_http_port_t *hport,
             && (addr[i].wc_head == NULL
                 || addr[i].wc_head->hash.buckets == NULL)
             && (addr[i].wc_tail == NULL
-                || addr[i].wc_tail->hash.buckets == NULL))
+                || addr[i].wc_tail->hash.buckets == NULL)
+#if (NGX_PCRE)
+            && addr[i].nregex == 0
+#endif
+            )
         {
             continue;
         }
