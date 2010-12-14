1db960a374ec3e56e6b117c0e5e877fd75f53922
Igor Sysoev <igor@sysoev.ru>
2010-12-14 10:41:22 +0000

    merge r3825, r3826:
    *) test wildcard tail hash existance for IPv6 addresses,
       the same fix for IPv4 addresses has been made in r2581
    *) always run regex in server_name to get captures for IPv6 addresses,
       the same fix for IPv4 addresses has been made in r2584

--- src/http/ngx_http.c
+++ src/http/ngx_http.c
@@ -1870,8 +1870,12 @@ ngx_http_add_addrs6(ngx_conf_t *cf, ngx_http_port_t *hport,
         if (addr[i].hash.buckets == NULL
             && (addr[i].wc_head == NULL
                 || addr[i].wc_head->hash.buckets == NULL)
-            && (addr[i].wc_head == NULL
-                || addr[i].wc_head->hash.buckets == NULL))
+            && (addr[i].wc_tail == NULL
+                || addr[i].wc_tail->hash.buckets == NULL)
+#if (NGX_PCRE)
+            && addr[i].nregex == 0
+#endif
+            )
         {
             continue;
         }
