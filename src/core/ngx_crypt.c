c4793b650579a0475e7d2e61d1530887962bdb7e
Igor Sysoev <igor@sysoev.ru>
2011-05-26 07:32:48 +0000

    fix building --without-http_auth_basic_module, the bug has been introduced in r3923

--- src/core/ngx_crypt.c
+++ src/core/ngx_crypt.c
@@ -12,6 +12,8 @@
 #endif
 
 
+#if (NGX_CRYPT)
+
 static ngx_int_t ngx_crypt_apr1(ngx_pool_t *pool, u_char *key, u_char *salt,
     u_char **encrypted);
 static ngx_int_t ngx_crypt_plain(ngx_pool_t *pool, u_char *key, u_char *salt,
@@ -232,3 +234,5 @@ ngx_crypt_ssha(ngx_pool_t *pool, u_char *key, u_char *salt, u_char **encrypted)
 }
 
 #endif /* NGX_HAVE_SHA1 */
+
+#endif /* NGX_CRYPT */
