2551f4a55ddcb60460fc65fe7a3466647581d277
Igor Sysoev <igor@sysoev.ru>
2011-07-20 09:29:25 +0000

    bump version: 1.1.0 development version

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1000005
-#define NGINX_VERSION      "1.0.5"
+#define nginx_version      1001000
+#define NGINX_VERSION      "1.1.0"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
