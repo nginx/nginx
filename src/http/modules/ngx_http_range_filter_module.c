nginx/nginx

commit ee4b59fd37c9ca2c7b003446adc3a2ce38b8df60

Now unsatisfiable ranges are processed according to RFC 2616.
git-svn-id: svn://svn.nginx.org/nginx/trunk@4057 73f98a42-aea0-e011-b76d-00259023448c

src/http/modules/ngx_http_range_filter_module.c
@@ -264,7 +264,7 @@ ngx_http_range_parse(ngx_http_request_t *r, ngx_http_range_filter_ctx_t *ctx)
             }
 
             if (start >= r->headers_out.content_length_n) {
-                return NGX_HTTP_RANGE_NOT_SATISFIABLE;
+                goto skip;
             }
 
             while (*p == ' ') { p++; }
@@ -299,14 +299,10 @@ ngx_http_range_parse(ngx_http_request_t *r, ngx_http_range_filter_ctx_t *ctx)
         }
 
         if (start > end) {
-            return NGX_HTTP_RANGE_NOT_SATISFIABLE;
+            goto skip;
         }
 
         if (end >= r->headers_out.content_length_n) {
-            /*
-             * Download Accelerator sends the last byte position
-             * that equals to the file length
-             */
             end = r->headers_out.content_length_n;
 
         } else {
@@ -325,9 +321,15 @@ ngx_http_range_parse(ngx_http_request_t *r, ngx_http_range_filter_ctx_t *ctx)
 
         size += end - start;
 
+    skip:
+
         if (*p++ != ',') {
             break;
         }
+    }
+
+    if (ctx->ranges.nelts == 0) {
+        return NGX_HTTP_RANGE_NOT_SATISFIABLE;
     }
 
     if (size > r->headers_out.content_length_n) {

commit 1f3280bae2b3a3e70471d382bdfa13a06a53eae0

Unsatisfiable range with start value greater than content length was not properly skipped. The bug has been introduced in r4057.

src/http/modules/ngx_http_range_filter_module.c
@@ -264,10 +264,6 @@ ngx_http_range_parse(ngx_http_request_t *r, ngx_http_range_filter_ctx_t *ctx)
                 return NGX_HTTP_RANGE_NOT_SATISFIABLE;
             }
 
-            if (start >= content_length) {
-                goto skip;
-            }
-
             while (*p == ' ') { p++; }
 
             if (*p == ',' || *p == '\0') {
@@ -299,7 +295,7 @@ ngx_http_range_parse(ngx_http_request_t *r, ngx_http_range_filter_ctx_t *ctx)
             end = content_length - 1;
         }
 
-        if (start > end) {
+        if (start >= content_length || start > end) {
             goto skip;
         }
 