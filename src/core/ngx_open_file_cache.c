9c5d25179cbda4968962fb2bd8dcbc8e417b7b85
Igor Sysoev <igor@sysoev.ru>
2011-09-14 14:28:55 +0000

    Fix of the previous commit: is_directio flag processing introduced in r4077 is still worthwhile.

--- src/core/ngx_open_file_cache.c
+++ src/core/ngx_open_file_cache.c
@@ -288,6 +288,8 @@ ngx_open_cached_file(ngx_open_file_cache_t *cache, ngx_str_t *name,
                     file->use_event = 1;
                 }
 
+                of->is_directio = file->is_directio;
+
                 goto update;
             }
 
