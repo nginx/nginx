ab0078a4c8626572282943915348daa6e0494b83
Igor Sysoev <igor@sysoev.ru>
2011-10-05 13:13:25 +0000

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1001004
-#define NGINX_VERSION      "1.1.4"
+#define nginx_version      1001005
+#define NGINX_VERSION      "1.1.5"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
--- src/http/modules/perl/nginx.pm
+++ src/http/modules/perl/nginx.pm
@@ -48,7 +48,7 @@ our @EXPORT = qw(
     HTTP_INSUFFICIENT_STORAGE
 );
 
-our $VERSION = '1.1.4';
+our $VERSION = '1.1.5';
 
 require XSLoader;
 XSLoader::load('nginx', $VERSION);
