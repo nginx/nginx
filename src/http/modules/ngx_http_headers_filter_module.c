1d13c0dd34d859d28ede439d248438a2288d39fc
Maxim Dounin <mdounin@mdounin.ru>
2011-11-28 10:00:47 +0000

    Allowed add_header for proxied 206 replies.
    It was working for nginx's own 206 replies as they are seen as 200 in the
    headers filter module (range filter goes later in the headers filter chain),
    but not for proxied replies.

--- src/http/modules/ngx_http_headers_filter_module.c
+++ src/http/modules/ngx_http_headers_filter_module.c
@@ -145,6 +145,7 @@ ngx_http_headers_filter(ngx_http_request_t *r)
         || r != r->main
         || (r->headers_out.status != NGX_HTTP_OK
             && r->headers_out.status != NGX_HTTP_NO_CONTENT
+            && r->headers_out.status != NGX_HTTP_PARTIAL_CONTENT
             && r->headers_out.status != NGX_HTTP_MOVED_PERMANENTLY
             && r->headers_out.status != NGX_HTTP_MOVED_TEMPORARILY
             && r->headers_out.status != NGX_HTTP_NOT_MODIFIED))
