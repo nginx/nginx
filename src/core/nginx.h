a0a8663eae881d954dc18c1fa00a6b51a92e441e
Maxim Dounin <mdounin@mdounin.ru>
2011-12-13 16:59:41 +0000

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1000010
-#define NGINX_VERSION      "1.0.10"
+#define nginx_version      1000011
+#define NGINX_VERSION      "1.0.11"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
