11f09dd07d58b0d9f26e36427c91bc026e48c34b
Maxim Dounin <mdounin@mdounin.ru>
2011-12-27 11:56:48 +0000

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1001011
-#define NGINX_VERSION      "1.1.12"
+#define nginx_version      1001013
+#define NGINX_VERSION      "1.1.13"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
--- src/http/modules/perl/nginx.pm
+++ src/http/modules/perl/nginx.pm
@@ -48,7 +48,7 @@ our @EXPORT = qw(
     HTTP_INSUFFICIENT_STORAGE
 );
 
-our $VERSION = '1.1.12';
+our $VERSION = '1.1.13';
 
 require XSLoader;
 XSLoader::load('nginx', $VERSION);
