6154497b908ce5812d1baba847bf6055f9801116
Maxim Dounin <mdounin@mdounin.ru>
2012-02-04 23:28:10 +0000

    Merge of r4327:
    Removed unused function ngx_regex_capture_count().  The function has
    been unused since r3326 (0.8.25).

--- src/core/ngx_regex.c
+++ src/core/ngx_regex.c
@@ -137,23 +137,6 @@ failed:
 
 
 ngx_int_t
-ngx_regex_capture_count(ngx_regex_t *re)
-{
-    int  rc, n;
-
-    n = 0;
-
-    rc = pcre_fullinfo(re, NULL, PCRE_INFO_CAPTURECOUNT, &n);
-
-    if (rc < 0) {
-        return (ngx_int_t) rc;
-    }
-
-    return (ngx_int_t) n;
-}
-
-
-ngx_int_t
 ngx_regex_exec_array(ngx_array_t *a, ngx_str_t *s, ngx_log_t *log)
 {
     ngx_int_t         n;
