a95c85e78ae87fe165f73d27a706469320ceeb0a
Maxim Dounin <mdounin@mdounin.ru>
2012-02-28 14:54:23 +0000

    Raised simultaneous subrequest limit from 50 to 200.
    It wasn't enforced for a long time, and there are reports that people
    use up to 100 simultaneous subrequests now.  As this is a safety limit
    to prevent loops, it's raised accordingly.

--- src/http/ngx_http_request.h
+++ src/http/ngx_http_request.h
@@ -10,7 +10,7 @@
 
 
 #define NGX_HTTP_MAX_URI_CHANGES           10
-#define NGX_HTTP_MAX_SUBREQUESTS           50
+#define NGX_HTTP_MAX_SUBREQUESTS           200
 
 /* must be 2^n */
 #define NGX_HTTP_LC_HEADER_LEN             32
