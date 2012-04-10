654a56e06a5d5b8a25bdbc8d5a84bd8b962de1cb
Igor Sysoev <igor@sysoev.ru>
2012-04-10 11:27:43 +0000

    Fixed previous commit.

--- src/http/modules/ngx_http_mp4_module.c
+++ src/http/modules/ngx_http_mp4_module.c
@@ -1910,7 +1910,7 @@ ngx_http_mp4_update_stts_atom(ngx_http_mp4_file_t *mp4,
 
         if (start_time < (uint64_t) count * duration) {
             start_sample += (ngx_uint_t) (start_time / duration);
-            count -= start_time;
+            count -= (uint32_t) (start_time / duration);
             ngx_mp4_set_32value(entry->count, count);
             goto found;
         }
