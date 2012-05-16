10c205a1b3641fe5cc68051467c82becb3af11de
Ruslan Ermilov <ru@nginx.com>
2012-05-16 13:14:53 +0000

    Added syntax checking of the second parameter of the "split_clients" directive.

--- src/http/modules/ngx_http_split_clients_module.c
+++ src/http/modules/ngx_http_split_clients_module.c
@@ -138,6 +138,13 @@ ngx_conf_split_clients_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     }
 
     name = value[2];
+
+    if (name.len < 2 || name.data[0] != '$') {
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "invalid variable name \"%V\"", &name);
+        return NGX_CONF_ERROR;
+    }
+
     name.len--;
     name.data++;
 
