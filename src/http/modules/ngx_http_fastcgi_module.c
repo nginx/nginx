86dd5bde4590e60289cf2ae94afe2f9ab5d65f3c
Valentin Bartenev <vbart@nginx.com>
2012-08-08 12:03:46 +0000

    Added three missing checks for NULL after ngx_array_push() calls.
    Found by Coverity.

--- src/http/modules/ngx_http_fastcgi_module.c
+++ src/http/modules/ngx_http_fastcgi_module.c
@@ -1626,6 +1626,9 @@ ngx_http_fastcgi_process_header(ngx_http_request_t *r)
         }
 
         part = ngx_array_push(f->split_parts);
+        if (part == NULL) {
+            return NGX_ERROR;
+        }
 
         part->start = part_start;
         part->end = part_end;
