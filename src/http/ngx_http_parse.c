dc5c6928fb0d45a80947b2a294f77a432985747a
Maxim Dounin <mdounin@mdounin.ru>
2013-06-05 19:45:08 +0400

    Fixed debug logging in ngx_http_parse_complex_uri().
    The *u previously logged isn't yet initialized at this point, and
    Valgrind complains.

--- src/http/ngx_http_parse.c
+++ src/http/ngx_http_parse.c
@@ -1256,8 +1256,8 @@ ngx_http_parse_complex_uri(ngx_http_request_t *r, ngx_uint_t merge_slashes)
          * the line feed
          */
 
-        ngx_log_debug4(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                       "s:%d in:'%Xd:%c', out:'%c'", state, ch, ch, *u);
+        ngx_log_debug3(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                       "s:%d in:'%Xd:%c'", state, ch, ch);
 
         switch (state) {
 
