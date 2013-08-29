3ef0dfa145513c98b331f75730c3f16e4713cda4
Sergey Kandaurov <pluknet@nginx.com>
2013-08-29 22:35:26 +0400

    Referer: fixed error type usage inconsistency for ngx_http_add*().

--- src/http/modules/ngx_http_referer_module.c
+++ src/http/modules/ngx_http_referer_module.c
@@ -41,9 +41,9 @@ static char * ngx_http_referer_merge_conf(ngx_conf_t *cf, void *parent,
     void *child);
 static char *ngx_http_valid_referers(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
-static char *ngx_http_add_referer(ngx_conf_t *cf, ngx_hash_keys_arrays_t *keys,
-    ngx_str_t *value, ngx_str_t *uri);
-static char *ngx_http_add_regex_referer(ngx_conf_t *cf,
+static ngx_int_t ngx_http_add_referer(ngx_conf_t *cf,
+    ngx_hash_keys_arrays_t *keys, ngx_str_t *value, ngx_str_t *uri);
+static ngx_int_t ngx_http_add_regex_referer(ngx_conf_t *cf,
     ngx_http_referer_conf_t *rlcf, ngx_str_t *name, ngx_regex_t *regex);
 static int ngx_libc_cdecl ngx_http_cmp_referer_wildcards(const void *one,
     const void *two);
@@ -497,7 +497,7 @@ ngx_http_valid_referers(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 }
 
 
-static char *
+static ngx_int_t
 ngx_http_add_referer(ngx_conf_t *cf, ngx_hash_keys_arrays_t *keys,
     ngx_str_t *value, ngx_str_t *uri)
 {
@@ -510,7 +510,7 @@ ngx_http_add_referer(ngx_conf_t *cf, ngx_hash_keys_arrays_t *keys,
     } else {
         u = ngx_palloc(cf->pool, sizeof(ngx_str_t));
         if (u == NULL) {
-            return NGX_CONF_ERROR;
+            return NGX_ERROR;
         }
 
         *u = *uri;
@@ -519,7 +519,7 @@ ngx_http_add_referer(ngx_conf_t *cf, ngx_hash_keys_arrays_t *keys,
     rc = ngx_hash_add_key(keys, value, u, NGX_HASH_WILDCARD_KEY);
 
     if (rc == NGX_OK) {
-        return NGX_CONF_OK;
+        return NGX_OK;
     }
 
     if (rc == NGX_DECLINED) {
@@ -532,11 +532,11 @@ ngx_http_add_referer(ngx_conf_t *cf, ngx_hash_keys_arrays_t *keys,
                            "conflicting parameter \"%V\"", value);
     }
 
-    return NGX_CONF_ERROR;
+    return NGX_ERROR;
 }
 
 
-static char *
+static ngx_int_t
 ngx_http_add_regex_referer(ngx_conf_t *cf, ngx_http_referer_conf_t *rlcf,
     ngx_str_t *name, ngx_regex_t *regex)
 {
@@ -547,26 +547,26 @@ ngx_http_add_regex_referer(ngx_conf_t *cf, ngx_http_referer_conf_t *rlcf,
 
     if (name->len == 1) {
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "empty regex in \"%V\"", name);
-        return NGX_CONF_ERROR;
+        return NGX_ERROR;
     }
 
     if (rlcf->regex == NGX_CONF_UNSET_PTR) {
         rlcf->regex = ngx_array_create(cf->pool, 2, sizeof(ngx_regex_elt_t));
         if (rlcf->regex == NULL) {
-            return NGX_CONF_ERROR;
+            return NGX_ERROR;
         }
     }
 
     re = ngx_array_push(rlcf->regex);
     if (re == NULL) {
-        return NGX_CONF_ERROR;
+        return NGX_ERROR;
     }
 
     if (regex) {
         re->regex = regex;
         re->name = name->data;
 
-        return NGX_CONF_OK;
+        return NGX_OK;
     }
 
     name->len--;
@@ -582,13 +582,13 @@ ngx_http_add_regex_referer(ngx_conf_t *cf, ngx_http_referer_conf_t *rlcf,
 
     if (ngx_regex_compile(&rc) != NGX_OK) {
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "%V", &rc.err);
-        return NGX_CONF_ERROR;
+        return NGX_ERROR;
     }
 
     re->regex = rc.regex;
     re->name = name->data;
 
-    return NGX_CONF_OK;
+    return NGX_OK;
 
 #else
 
@@ -596,7 +596,7 @@ ngx_http_add_regex_referer(ngx_conf_t *cf, ngx_http_referer_conf_t *rlcf,
                        "the using of the regex \"%V\" requires PCRE library",
                        name);
 
-    return NGX_CONF_ERROR;
+    return NGX_ERROR;
 
 #endif
 }
