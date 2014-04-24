58e26b88b774bbffbcd54bfc2de57a1b902ed4dd
Valentin Bartenev <vbart@nginx.com>
2014-04-24 20:50:10 +0400

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1007000
-#define NGINX_VERSION      "1.7.0"
+#define nginx_version      1007001
+#define NGINX_VERSION      "1.7.1"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
