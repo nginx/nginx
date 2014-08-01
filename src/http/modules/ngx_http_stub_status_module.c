88132eed5438f57b61660687c5037e2643a38bed
Sergey Kandaurov <pluknet@nginx.com>
2014-08-01 18:36:35 +0400

    Status: indentation and style, no functional changes.

--- src/http/modules/ngx_http_stub_status_module.c
+++ src/http/modules/ngx_http_stub_status_module.c
@@ -10,25 +10,25 @@
 #include <ngx_http.h>
 
 
+static ngx_int_t ngx_http_stub_status_handler(ngx_http_request_t *r);
 static ngx_int_t ngx_http_stub_status_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
 static ngx_int_t ngx_http_stub_status_add_variables(ngx_conf_t *cf);
+static char *ngx_http_set_stub_status(ngx_conf_t *cf, ngx_command_t *cmd,
+    void *conf);
 
-static char *ngx_http_set_status(ngx_conf_t *cf, ngx_command_t *cmd,
-                                 void *conf);
 
 static ngx_command_t  ngx_http_status_commands[] = {
 
     { ngx_string("stub_status"),
       NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
-      ngx_http_set_status,
+      ngx_http_set_stub_status,
       0,
       0,
       NULL },
 
       ngx_null_command
 };
-
 
 
 static ngx_http_module_t  ngx_http_stub_status_module_ctx = {
@@ -80,7 +80,8 @@ static ngx_http_variable_t  ngx_http_stub_status_vars[] = {
 };
 
 
-static ngx_int_t ngx_http_status_handler(ngx_http_request_t *r)
+static ngx_int_t
+ngx_http_stub_status_handler(ngx_http_request_t *r)
 {
     size_t             size;
     ngx_int_t          rc;
@@ -223,12 +224,13 @@ ngx_http_stub_status_add_variables(ngx_conf_t *cf)
 }
 
 
-static char *ngx_http_set_status(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+static char *
+ngx_http_set_stub_status(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_http_core_loc_conf_t  *clcf;
 
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_status_handler;
+    clcf->handler = ngx_http_stub_status_handler;
 
     return NGX_CONF_OK;
 }
