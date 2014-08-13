0a02bdb24932d0b026f078b21d69b9efc0e04599
Valentin Bartenev <vbart@nginx.com>
2014-08-13 15:11:45 +0400

    Merged implementations of ngx_readv_chain().
    There's no real need in two separate implementations,
    with and without kqueue support.

--- src/os/unix/ngx_readv_chain.c
+++ src/os/unix/ngx_readv_chain.c
@@ -17,8 +17,6 @@
 #endif
 
 
-#if (NGX_HAVE_KQUEUE)
-
 ssize_t
 ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
 {
@@ -30,6 +28,8 @@ ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
     struct iovec  *iov, iovs[NGX_IOVS];
 
     rev = c->read;
+
+#if (NGX_HAVE_KQUEUE)
 
     if (ngx_event_flags & NGX_USE_KQUEUE_EVENT) {
         ngx_log_debug3(NGX_LOG_DEBUG_EVENT, c->log, 0,
@@ -57,6 +57,8 @@ ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
             }
         }
     }
+
+#endif
 
     prev = NULL;
     iov = NULL;
@@ -102,6 +104,9 @@ ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
         n = readv(c->fd, (struct iovec *) vec.elts, vec.nelts);
 
         if (n >= 0) {
+
+#if (NGX_HAVE_KQUEUE)
+
             if (ngx_event_flags & NGX_USE_KQUEUE_EVENT) {
                 rev->available -= n;
 
@@ -141,6 +146,8 @@ ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
                 return n;
             }
 
+#endif /* NGX_HAVE_KQUEUE */
+
             if (n < size && !(ngx_event_flags & NGX_USE_GREEDY_EVENT)) {
                 rev->ready = 0;
             }
@@ -174,98 +181,3 @@ ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
 
     return n;
 }
-
-#else /* ! NGX_HAVE_KQUEUE */
-
-ssize_t
-ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *chain)
-{
-    u_char        *prev;
-    ssize_t        n, size;
-    ngx_err_t      err;
-    ngx_array_t    vec;
-    ngx_event_t   *rev;
-    struct iovec  *iov, iovs[NGX_IOVS];
-
-    prev = NULL;
-    iov = NULL;
-    size = 0;
-
-    vec.elts = iovs;
-    vec.nelts = 0;
-    vec.size = sizeof(struct iovec);
-    vec.nalloc = NGX_IOVS;
-    vec.pool = c->pool;
-
-    /* coalesce the neighbouring bufs */
-
-    while (chain) {
-        if (prev == chain->buf->last) {
-            iov->iov_len += chain->buf->end - chain->buf->last;
-
-        } else {
-            if (vec.nelts >= IOV_MAX) {
-                break;
-            }
-
-            iov = ngx_array_push(&vec);
-            if (iov == NULL) {
-                return NGX_ERROR;
-            }
-
-            iov->iov_base = (void *) chain->buf->last;
-            iov->iov_len = chain->buf->end - chain->buf->last;
-        }
-
-        size += chain->buf->end - chain->buf->last;
-        prev = chain->buf->end;
-        chain = chain->next;
-    }
-
-    ngx_log_debug2(NGX_LOG_DEBUG_EVENT, c->log, 0,
-                   "readv: %d:%d", vec.nelts, iov->iov_len);
-
-    rev = c->read;
-
-    do {
-        n = readv(c->fd, (struct iovec *) vec.elts, vec.nelts);
-
-        if (n == 0) {
-            rev->ready = 0;
-            rev->eof = 1;
-
-            return n;
-
-        } else if (n > 0) {
-
-            if (n < size && !(ngx_event_flags & NGX_USE_GREEDY_EVENT)) {
-                rev->ready = 0;
-            }
-
-            return n;
-        }
-
-        err = ngx_socket_errno;
-
-        if (err == NGX_EAGAIN || err == NGX_EINTR) {
-            ngx_log_debug0(NGX_LOG_DEBUG_EVENT, c->log, err,
-                           "readv() not ready");
-            n = NGX_AGAIN;
-
-        } else {
-            n = ngx_connection_error(c, err, "readv() failed");
-            break;
-        }
-
-    } while (err == NGX_EINTR);
-
-    rev->ready = 0;
-
-    if (n == NGX_ERROR) {
-        c->read->error = 1;
-    }
-
-    return n;
-}
-
-#endif /* NGX_HAVE_KQUEUE */
