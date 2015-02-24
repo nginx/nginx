7dd799357d7fd853b488b52ae17f9d0ee3142941
Maxim Dounin <mdounin@mdounin.ru>
2015-02-24 18:37:14 +0300

    Core: fixed potential buffer overrun when initializing hash.
    Initial size as calculated from the number of elements may be bigger
    than max_size.  If this happens, make sure to set size to max_size.
    
    Reported by Chris West.

--- src/core/ngx_hash.c
+++ src/core/ngx_hash.c
@@ -312,7 +312,7 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
         continue;
     }
 
-    size--;
+    size = hinit->max_size;
 
     ngx_log_error(NGX_LOG_WARN, hinit->pool->log, 0,
                   "could not build optimal %s, you should increase "
