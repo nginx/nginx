3de18022d862b120311696e2c58861a4ac6a928d
Ruslan Ermilov <ru@nginx.com>
2015-05-29 09:26:27 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1009001
-#define NGINX_VERSION      "1.9.1"
+#define nginx_version      1009002
+#define NGINX_VERSION      "1.9.2"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
