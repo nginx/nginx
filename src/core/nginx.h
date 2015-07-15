3cc600087999a69e30905b272cf17054e57d399d
Maxim Dounin <mdounin@mdounin.ru>
2015-07-15 22:56:03 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1009003
-#define NGINX_VERSION      "1.9.3"
+#define nginx_version      1009004
+#define NGINX_VERSION      "1.9.4"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
