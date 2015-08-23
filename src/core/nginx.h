fae2488df708e7a2e5cc8d418bad2985c08d1ecb
Valentin Bartenev <vbart@nginx.com>
2015-08-23 21:03:29 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1009004
-#define NGINX_VERSION      "1.9.4"
+#define nginx_version      1009005
+#define NGINX_VERSION      "1.9.5"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
