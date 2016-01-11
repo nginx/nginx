af647a3da2eb9ff558655424868f82f259de94f1
Maxim Dounin <mdounin@mdounin.ru>
2016-01-11 19:08:12 +0300

    Upstream: fixed changing method on X-Accel-Redirect.
    Previously, only r->method was changed, resulting in handling of a request
    as GET within nginx itself, but not in requests to proxied servers.
    
    See http://mailman.nginx.org/pipermail/nginx/2015-December/049518.html.

--- src/http/ngx_http_upstream.c
+++ src/http/ngx_http_upstream.c
@@ -2499,6 +2499,7 @@ ngx_http_upstream_process_headers(ngx_http_request_t *r, ngx_http_upstream_t *u)
 
             if (r->method != NGX_HTTP_HEAD) {
                 r->method = NGX_HTTP_GET;
+                r->method_name = ngx_http_core_get_method;
             }
 
             ngx_http_internal_redirect(r, &uri, &args);
