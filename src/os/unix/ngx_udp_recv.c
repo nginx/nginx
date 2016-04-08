8e7d8757c656b9d8f0af7953716929c97657474f
Valentin Bartenev <vbart@nginx.com>
2016-04-08 16:38:42 +0300

    Merged implementations of ngx_udp_unix_recv().
    There's no real need in two separate implementations,
    with and without kqueue support.

--- src/os/unix/ngx_udp_recv.c
+++ src/os/unix/ngx_udp_recv.c
@@ -10,8 +10,6 @@
 #include <ngx_event.h>
 
 
-#if (NGX_HAVE_KQUEUE)
-
 ssize_t
 ngx_udp_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
 {
@@ -28,6 +26,9 @@ ngx_udp_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
                        "recv: fd:%d %z of %uz", c->fd, n, size);
 
         if (n >= 0) {
+
+#if (NGX_HAVE_KQUEUE)
+
             if (ngx_event_flags & NGX_USE_KQUEUE_EVENT) {
                 rev->available -= n;
 
@@ -42,6 +43,8 @@ ngx_udp_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
                 }
             }
 
+#endif
+
             return n;
         }
 
@@ -67,49 +70,3 @@ ngx_udp_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
 
     return n;
 }
-
-#else /* ! NGX_HAVE_KQUEUE */
-
-ssize_t
-ngx_udp_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
-{
-    ssize_t       n;
-    ngx_err_t     err;
-    ngx_event_t  *rev;
-
-    rev = c->read;
-
-    do {
-        n = recv(c->fd, buf, size, 0);
-
-        ngx_log_debug3(NGX_LOG_DEBUG_EVENT, c->log, 0,
-                       "recv: fd:%d %z of %uz", c->fd, n, size);
-
-        if (n >= 0) {
-            return n;
-        }
-
-        err = ngx_socket_errno;
-
-        if (err == NGX_EAGAIN || err == NGX_EINTR) {
-            ngx_log_debug0(NGX_LOG_DEBUG_EVENT, c->log, err,
-                           "recv() not ready");
-            n = NGX_AGAIN;
-
-        } else {
-            n = ngx_connection_error(c, err, "recv() failed");
-            break;
-        }
-
-    } while (err == NGX_EINTR);
-
-    rev->ready = 0;
-
-    if (n == NGX_ERROR) {
-        rev->error = 1;
-    }
-
-    return n;
-}
-
-#endif /* NGX_HAVE_KQUEUE */
