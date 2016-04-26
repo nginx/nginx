edeb561fe17099a8e8bf134f9f2180652bcd2bf0
Maxim Dounin <mdounin@mdounin.ru>
2016-04-26 16:30:30 +0300

    Stable branch.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1009015
-#define NGINX_VERSION      "1.9.15"
+#define nginx_version      1010000
+#define NGINX_VERSION      "1.10.0"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
