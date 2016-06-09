ab8504b937cdbae734fb2d971fba4eea0b157f43
Maxim Dounin <mdounin@mdounin.ru>
2016-06-09 16:55:38 +0300

    An internal SHA1 implementation.

--- src/core/ngx_sha1.h
+++ src/core/ngx_sha1.h
@@ -13,6 +13,8 @@
 #include <ngx_core.h>
 
 
+#if (NGX_HAVE_SHA1)
+
 #if (NGX_HAVE_OPENSSL_SHA1_H)
 #include <openssl/sha.h>
 #else
@@ -27,5 +29,22 @@ typedef SHA_CTX  ngx_sha1_t;
 #define ngx_sha1_update  SHA1_Update
 #define ngx_sha1_final   SHA1_Final
 
+
+#else /* !NGX_HAVE_SHA1 */
+
+
+typedef struct {
+    uint64_t  bytes;
+    uint32_t  a, b, c, d, e, f;
+    u_char    buffer[64];
+} ngx_sha1_t;
+
+
+void ngx_sha1_init(ngx_sha1_t *ctx);
+void ngx_sha1_update(ngx_sha1_t *ctx, const void *data, size_t size);
+void ngx_sha1_final(u_char result[20], ngx_sha1_t *ctx);
+
+
+#endif
 
 #endif /* _NGX_SHA1_H_INCLUDED_ */
