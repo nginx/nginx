b9a031622b7bf586a9c724dade16abfac188f2f2
Roman Arutyunyan <arut@nginx.com>
2017-02-10 16:33:12 +0300

    Slice filter: fetch slices in cloned subrequests.
    Previously, slice subrequest location was selected based on request URI.
    If request is then redirected to a new location, its context array is cleared,
    making the slice module loose current slice range information.  This lead to
    broken output.  Now subrequests with the NGX_HTTP_SUBREQUEST_CLONE flag are
    created for slices.  Such subrequests stay in the same location as the parent
    request and keep the right slice context.

--- src/http/modules/ngx_http_slice_filter_module.c
+++ src/http/modules/ngx_http_slice_filter_module.c
@@ -244,7 +244,10 @@ ngx_http_slice_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
         return rc;
     }
 
-    if (ngx_http_subrequest(r, &r->uri, &r->args, &sr, NULL, 0) != NGX_OK) {
+    if (ngx_http_subrequest(r, &r->uri, &r->args, &sr, NULL,
+                            NGX_HTTP_SUBREQUEST_CLONE)
+        != NGX_OK)
+    {
         return NGX_ERROR;
     }
 
