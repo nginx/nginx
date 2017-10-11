211d20a23021a3d45608c6075a92c73319715620
Ruslan Ermilov <ru@nginx.com>
2017-10-11 22:04:11 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1013006
-#define NGINX_VERSION      "1.13.6"
+#define nginx_version      1013007
+#define NGINX_VERSION      "1.13.7"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
