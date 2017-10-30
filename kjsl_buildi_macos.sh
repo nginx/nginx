5f8eb0de523d7a0ef7e0f66382bf17aea1746753
klambert <klambert@switch.tv>
2017-10-31 10:14:34 +1100

    Adding RTMP

--- kjsl_buildi_macos.sh
+++ kjsl_buildi_macos.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
+git clone https://github.com/kevleyski/nginx-rtmp-module nginx-rtmp-module
+git clone https://github.com/kevleyski/ngx_devel_kit ngx_devel_kit 
+git clone https://github.com/kevleyski/lua-nginx-module.git lua-nginx-module
 mkdir -p /usr/local/nginx
 mkdir -p /usr/local/nginx/nginx
-./auto/configure
+./auto/configure --prefix=/etc/nginx \
+	--prefix=/etc/nginx \
+	--sbin-path=/usr/sbin/nginx \
+	--conf-path=/etc/nginx/nginx.conf \
+	--error-log-path=/var/log/nginx/error.log \
+	--http-log-path=/var/log/nginx/access.log \
+	--pid-path=/var/run/nginx.pid \
+	--lock-path=/var/run/nginx.lock \
+	--build=CentOS \
+	--http-client-body-temp-path=/var/cache/nginx/client_temp \
+	--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+	--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+	--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+	--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+	--user=nginx \
+	--group=nginx \
+	--with-http_ssl_module \
+	--with-http_realip_module \
+	--with-http_addition_module \
+	--with-http_sub_module \
+	--with-http_dav_module \
+	--with-http_flv_module \
+	--with-http_mp4_module \
+	--with-http_gunzip_module \
+	--with-http_gzip_static_module \
+	--with-http_random_index_module \
+	--with-http_secure_link_module \
+	--with-http_stub_status_module \
+	--with-http_auth_request_module \
+	--with-threads \
+	--with-stream \
+	--with-stream_ssl_module \
+	--with-http_slice_module \
+	--with-mail \
+	--with-mail_ssl_module \
+	--with-file-aio \
+	--add-module=lua-nginx-module \
+	--add-module=ngx_devel_kit \
+ 	--add-module=nginx-rtmp-module
+	--with-ipv6 \
+	--with-http_v2_module \
+	--with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic' \
+
 make -j9
 sudo make install
+
+nginx -V
