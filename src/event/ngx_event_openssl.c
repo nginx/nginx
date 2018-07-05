34e524a130abb4e5ff549652e4a08e040b54dc57
Maxim Dounin <mdounin@mdounin.ru>
2018-07-05 20:45:29 +0300

    SSL: logging level of "https proxy request" errors.
    The "http request" and "https proxy request" errors cannot happen
    with HTTP due to pre-handshake checks in ngx_http_ssl_handshake(),
    but can happen when SSL is used in stream and mail modules.

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -2063,6 +2063,8 @@ ngx_ssl_connection_error(ngx_connection_t *c, int sslerr, ngx_err_t err,
             || n == SSL_R_DIGEST_CHECK_FAILED                        /*  149 */
             || n == SSL_R_ERROR_IN_RECEIVED_CIPHER_LIST              /*  151 */
             || n == SSL_R_EXCESSIVE_MESSAGE_SIZE                     /*  152 */
+            || n == SSL_R_HTTPS_PROXY_REQUEST                        /*  155 */
+            || n == SSL_R_HTTP_REQUEST                               /*  156 */
             || n == SSL_R_LENGTH_MISMATCH                            /*  159 */
 #ifdef SSL_R_NO_CIPHERS_PASSED
             || n == SSL_R_NO_CIPHERS_PASSED                          /*  182 */
