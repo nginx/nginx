3bd2b87a1d093b66bb3e354a276fa2ddaf244196
Kevin Staunton-Lambert <kevleyski@gmail.com>
2018-08-24 14:43:20 +1000

    deps

--- kjsl_buildi_macos.sh
+++ kjsl_buildi_macos.sh
@@ -2,40 +2,49 @@
 git clone https://github.com/kevleyski/nginx-rtmp-module nginx-rtmp-module
 git clone https://github.com/kevleyski/ngx_devel_kit ngx_devel_kit 
 
+if [ ! -d openssl-1.0.2o ]; then
 wget http://www.openssl.org/source/openssl-1.0.2o.tar.gz
 tar -zxf openssl-1.0.2o.tar.gz
 cd openssl-1.0.2o
 ./Configure darwin64-x86_64-cc --prefix=/usr
 make
 sudo make install
+cd ..
+fi
 
+if [ ! -d zlib-1.2.11 ]; then
 wget http://zlib.net/zlib-1.2.11.tar.gz
 tar -zxf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure
 make
 sudo make install
+cd ..
+fi
 
+if [ ! -d pcre-8.42 ]; then
 wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz
 tar -zxf pcre-8.42.tar.gz
 cd pcre-8.42
 ./configure
 make
 sudo make install
+cd ..
+fi
 
 mkdir -p /usr/local/nginx
 mkdir -p /usr/local/nginx/nginx
-./auto/configure --prefix=/etc/nginx \
-	--prefix=/etc/nginx \
+./auto/configure --prefix=/usr/local/etc/nginx \
+	--prefix=/usr/local/etc/nginx \
 	--sbin-path=/usr/sbin/nginx \
 	--conf-path=/etc/nginx/nginx.conf \
 	--error-log-path=/var/log/nginx/error.log \
 	--http-log-path=/var/log/nginx/access.log \
 	--pid-path=/var/run/nginx.pid \
 	--lock-path=/var/run/nginx.lock \
-	--with-pcre=../pcre-8.41 \
-	--with-zlib=../zlib-1.2.11 \
-	--with-openssl=../openss-1.0.2o \
+	--with-pcre=/Users/klambert/workspace/nginx/pcre-8.42 \
+	--with-zlib=/Users/klambert/workspace/nginx/zlib-1.2.11 \
+	--with-openssl=/Users/klambert/workspace/nginx/openssl-1.0.2o \
 	--with-http_ssl_module \
 	--http-client-body-temp-path=/var/cache/nginx/client_temp \
 	--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
