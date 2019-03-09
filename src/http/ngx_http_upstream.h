45ba5539372fd6890c2a223ab543621c4e00aeac
amirhossein.vz <hosseinvadi7777@gmail.com>
2019-03-09 15:27:03 +0330

    sudpport variable poxy next upstream directive

--- src/http/ngx_http_upstream.h
+++ src/http/ngx_http_upstream.h
@@ -171,6 +171,7 @@ typedef struct {
     ngx_uint_t                       ignore_headers;
     ngx_http_complex_value_t         *ar_ignore_headers;
     ngx_uint_t                       next_upstream;
+    ngx_http_complex_value_t         *ar_next_upstream;
     ngx_uint_t                       store_access;
     ngx_uint_t                       next_upstream_tries;
     ngx_flag_t                       buffering;
