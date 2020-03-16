1688f575c2d84ce0cf0fb6fe6558e1b308358ffd
Ruslan Ermilov <ru@nginx.com>
2020-03-16 12:41:41 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1017009
-#define NGINX_VERSION      "1.17.9"
+#define nginx_version      1017010
+#define NGINX_VERSION      "1.17.10"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
