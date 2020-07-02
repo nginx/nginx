86e287a2de91dfc0c7f6acc4220c15debd40a74d
Roman Arutyunyan <arut@nginx.com>
2020-07-02 16:33:36 +0300

    HTTP/3: downgraded literal size error level to NGX_LOG_INFO.
    Now it's similar to HTTP/2.

--- src/http/v3/ngx_http_v3_parse.c
+++ src/http/v3/ngx_http_v3_parse.c
@@ -446,7 +446,7 @@ ngx_http_v3_parse_literal(ngx_connection_t *c, ngx_http_v3_parse_literal_t *st,
         v3cf = ngx_http_v3_get_module_srv_conf(c, ngx_http_v3_module);
 
         if (n > v3cf->max_field_size) {
-            ngx_log_error(NGX_LOG_ERR, c->log, 0,
+            ngx_log_error(NGX_LOG_INFO, c->log, 0,
                           "client exceeded http3_max_field_size limit");
             return NGX_ERROR;
         }
