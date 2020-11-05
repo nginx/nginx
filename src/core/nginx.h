19795b763f41b60230c0a525256c5c3507ce3a7e
Vladimir Homutov <vl@nginx.com>
2020-11-05 22:37:27 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1019004
-#define NGINX_VERSION      "1.19.4"
+#define nginx_version      1019005
+#define NGINX_VERSION      "1.19.5"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
