2ec8fac2d6b8e5023c9894d10bc50d78e63f242c
Maxim Dounin <mdounin@mdounin.ru>
2021-01-19 20:32:00 +0300

    Core: removed post_accept_timeout.
    Keeping post_accept_timeout in ngx_listening_t is no longer needed since
    we've switched to 1 second timeout for deferred accept in 5541:fdb67cfc957d.
    
    Further, using it in HTTP code can result in client_header_timeout being
    used from an incorrect server block, notably if address-specific virtual
    servers are used along with a wildcard listening socket, or if we've switched
    to a different server block based on SNI in SSL handshake.

--- src/http/ngx_http.c
+++ src/http/ngx_http.c
@@ -1714,7 +1714,6 @@ ngx_http_add_listening(ngx_conf_t *cf, ngx_http_conf_addr_t *addr)
 
     cscf = addr->default_server;
     ls->pool_size = cscf->connection_pool_size;
-    ls->post_accept_timeout = cscf->client_header_timeout;
 
     clcf = cscf->ctx->loc_conf[ngx_http_core_module.ctx_index];
 
