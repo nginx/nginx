6150bc9e8075d748d20ffb7b90b8c2eb0f544728
Maxim Dounin <mdounin@mdounin.ru>
2021-02-11 21:52:23 +0300

    HTTP/2: removed http2_idle_timeout and http2_max_requests.
    Instead, keepalive_timeout and keepalive_requests are now used.  This
    is expected to simplify HTTP/2 code and usage.  This also matches
    directives used by upstream module for all protocols.
    
    In case of default settings, this effectively changes maximum number
    of requests per connection from 1000 to 100.  This looks acceptable,
    especially given that HTTP/2 code now properly supports lingering close.
    
    Further, this changes default keepalive timeout in HTTP/2 from 300 seconds
    to 75 seconds.  This also looks acceptable, and larger than PING interval
    used by Firefox (network.http.spdy.ping-threshold defaults to 58s),
    the only browser to use PINGs.

--- src/http/v2/ngx_http_v2_module.h
+++ src/http/v2/ngx_http_v2_module.h
@@ -24,12 +24,10 @@ typedef struct {
     size_t                          pool_size;
     ngx_uint_t                      concurrent_streams;
     ngx_uint_t                      concurrent_pushes;
-    ngx_uint_t                      max_requests;
     size_t                          max_field_size;
     size_t                          max_header_size;
     size_t                          preread_size;
     ngx_uint_t                      streams_index_mask;
-    ngx_msec_t                      idle_timeout;
 } ngx_http_v2_srv_conf_t;
 
 
