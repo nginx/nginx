fb88d79fa452afead54ca131408d86cca93b5de7
Maxim Dounin <mdounin@mdounin.ru>
2021-03-05 17:16:29 +0300

    Mail: realip module.
    When configured with the "set_real_ip_from", it can set client's IP
    address as visible in logs to the one obtained via the PROXY protocol.

--- src/mail/ngx_mail.h
+++ src/mail/ngx_mail.h
@@ -511,6 +511,7 @@ char *ngx_mail_capabilities(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 /* STUB */
 void ngx_mail_proxy_init(ngx_mail_session_t *s, ngx_addr_t *peer);
 void ngx_mail_auth_http_init(ngx_mail_session_t *s);
+ngx_int_t ngx_mail_realip_handler(ngx_mail_session_t *s);
 /**/
 
 ngx_mail_cleanup_t * ngx_mail_cleanup_add(ngx_mail_session_t * s, size_t size);
