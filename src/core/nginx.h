473ef4f869e9e2c0bd42f66d6c0ba252f50dfe3c
Maxim Dounin <mdounin@mdounin.ru>
2021-05-25 15:29:54 +0300

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1020000
-#define NGINX_VERSION      "1.20.0"
+#define nginx_version      1020001
+#define NGINX_VERSION      "1.20.1"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
