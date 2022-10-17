35fce42269bf1c84eadef6660021cefa08a960d7
Sergey Kandaurov <pluknet@nginx.com>
2022-10-17 16:24:53 +0400

    SSL: improved validation of ssl_session_cache and ssl_ocsp_cache.
    Now it properly detects invalid shared zone configuration with omitted size.
    Previously it used to read outside of the buffer boundary.
    
    Found with AddressSanitizer.

--- src/http/modules/ngx_http_ssl_module.c
+++ src/http/modules/ngx_http_ssl_module.c
@@ -1093,7 +1093,7 @@ ngx_http_ssl_session_cache(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
                 len++;
             }
 
-            if (len == 0) {
+            if (len == 0 || j == value[i].len) {
                 goto invalid;
             }
 
@@ -1183,7 +1183,7 @@ ngx_http_ssl_ocsp_cache(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         len++;
     }
 
-    if (len == 0) {
+    if (len == 0 || j == value[1].len) {
         goto invalid;
     }
 
