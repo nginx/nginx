c2f2b313a7f6912820eb17a97b8faded0e1d8079
Sergey Kandaurov <pluknet@nginx.com>
2023-04-17 14:06:43 +0400

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1023004
-#define NGINX_VERSION      "1.23.4"
+#define nginx_version      1025000
+#define NGINX_VERSION      "1.25.0"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
