ebd18ec1812bd6f3de54d9f9fc81563a0ec9f264
Sergey Kandaurov <pluknet@nginx.com>
2024-10-09 20:28:00 +0400

    SSL: disabled TLSv1 and TLSv1.1 by default.
    TLSv1 and TLSv1.1 are formally deprecated and forbidden to negotiate due
    to insufficient security reasons outlined in RFC 8996.
    
    TLSv1 and TLSv1.1 are disabled in BoringSSL e95b0cad9 and LibreSSL 3.8.1
    in the way they cannot be enabled in nginx configuration.  In OpenSSL 3.0,
    they are only permitted at security level 0 (disabled by default).
    
    The support is dropped in Chrome 84, Firefox 78, and deprecated in Safari.
    
    This change disables TLSv1 and TLSv1.1 by default for OpenSSL 1.0.1 and
    newer, where TLSv1.2 support is available.  For older library versions,
    which do not have alternatives, these protocol versions remain enabled.

--- src/mail/ngx_mail_ssl_module.c
+++ src/mail/ngx_mail_ssl_module.c
@@ -345,7 +345,9 @@ ngx_mail_ssl_merge_conf(ngx_conf_t *cf, void *parent, void *child)
 
     ngx_conf_merge_bitmask_value(conf->protocols, prev->protocols,
                          (NGX_CONF_BITMASK_SET
+#ifndef SSL_OP_NO_TLSv1_2
                           |NGX_SSL_TLSv1|NGX_SSL_TLSv1_1
+#endif
                           |NGX_SSL_TLSv1_2|NGX_SSL_TLSv1_3));
 
     ngx_conf_merge_uint_value(conf->verify, prev->verify, 0);
