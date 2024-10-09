d4a9abaffbf072ba656bf8df2c8f6f62977495f8
Sergey Kandaurov <pluknet@nginx.com>
2024-10-09 20:28:00 +0400

    SSL: disabled TLSv1 and TLSv1.1 by default.
    These TLS protocol versions are formally deprecated and forbidden to
    negotiate due to insufficient security reasons outlined in RFC 8996.
    
    The support was dropped in Chrome 84, Firefox 78.
    
    TLSv1 and TLSv1.1 are disabled in BoringSSL e95b0cad9 and LibreSSL 3.8.1
    in the way they cannot be enabled in nginx configuration.  In OpenSSL 3.0,
    they are only permitted at security level 0.

--- src/http/modules/ngx_http_ssl_module.c
+++ src/http/modules/ngx_http_ssl_module.c
@@ -653,7 +653,6 @@ ngx_http_ssl_merge_srv_conf(ngx_conf_t *cf, void *parent, void *child)
 
     ngx_conf_merge_bitmask_value(conf->protocols, prev->protocols,
                          (NGX_CONF_BITMASK_SET
-                          |NGX_SSL_TLSv1|NGX_SSL_TLSv1_1
                           |NGX_SSL_TLSv1_2|NGX_SSL_TLSv1_3));
 
     ngx_conf_merge_size_value(conf->buffer_size, prev->buffer_size,
