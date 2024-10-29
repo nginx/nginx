c5632521197ffba71ac322dee37bcb44e9c26c52
Amin Vakil <info@aminvakil.com>
2024-10-29 19:58:18 +0330

    Add http_401 to proxy_next_upstream

--- src/http/modules/ngx_http_proxy_module.c
+++ src/http/modules/ngx_http_proxy_module.c
@@ -263,6 +263,7 @@ static ngx_conf_bitmask_t  ngx_http_proxy_next_upstream_masks[] = {
     { ngx_string("http_502"), NGX_HTTP_UPSTREAM_FT_HTTP_502 },
     { ngx_string("http_503"), NGX_HTTP_UPSTREAM_FT_HTTP_503 },
     { ngx_string("http_504"), NGX_HTTP_UPSTREAM_FT_HTTP_504 },
+    { ngx_string("http_401"), NGX_HTTP_UPSTREAM_FT_HTTP_401 },
     { ngx_string("http_403"), NGX_HTTP_UPSTREAM_FT_HTTP_403 },
     { ngx_string("http_404"), NGX_HTTP_UPSTREAM_FT_HTTP_404 },
     { ngx_string("http_429"), NGX_HTTP_UPSTREAM_FT_HTTP_429 },
