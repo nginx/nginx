nginx/nginx

commit 58b92177e7c3c50f77f807ab3846ad5c7bbf0ebe

QUIC: discarding 0-RTT keys.
For simplicity, this is done on successful decryption of a 1-RTT packet.

src/event/quic/ngx_event_quic.c
@@ -1022,6 +1022,16 @@ ngx_quic_handle_payload(ngx_connection_t *c, ngx_quic_header_t *pkt)
         }
     }
 
+    if (pkt->level == ssl_encryption_application) {
+        /*
+         * RFC 9001, 4.9.3.  Discarding 0-RTT Keys
+         *
+         * After receiving a 1-RTT packet, servers MUST discard
+         * 0-RTT keys within a short time
+         */
+        ngx_quic_discard_ctx(c, ssl_encryption_early_data);
+    }
+
     if (qc->closing) {
         /*
          * RFC 9000, 10.2.  Immediate Close

commit 5f85b03d5d835040812f85d3f78118cd14a8344d

QUIC: fixed client request timeout in 0-RTT scenarios.
Since 0-RTT and 1-RTT data exist in the same packet number space,
ngx_quic_discard_ctx incorrectly discards 1-RTT packets when
0-RTT keys are discarded.

The issue was introduced by 58b92177e7c3c50f77f807ab3846ad5c7bbf0ebe.

src/event/quic/ngx_event_quic.c
@@ -1029,7 +1029,11 @@ ngx_quic_handle_payload(ngx_connection_t *c, ngx_quic_header_t *pkt)
          * After receiving a 1-RTT packet, servers MUST discard
          * 0-RTT keys within a short time
          */
-        ngx_quic_discard_ctx(c, ssl_encryption_early_data);
+        if (ngx_quic_keys_available(qc->keys, ssl_encryption_early_data, 0)
+            || ngx_quic_keys_available(qc->keys, ssl_encryption_early_data, 1))
+        {
+            ngx_quic_keys_discard(qc->keys, ssl_encryption_early_data);
+        }
     }
 
     if (qc->closing) {