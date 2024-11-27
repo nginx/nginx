e434398aacf13d40fd1fde405cc3b0edaf73be31
Roman Arutyunyan <arut@nginx.com>
2024-11-27 20:01:26 +0400

    Version bump.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1027003
-#define NGINX_VERSION      "1.27.3"
+#define nginx_version      1027004
+#define NGINX_VERSION      "1.27.4"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
