9e47ac7c929d1b60bb289170d2e98e0914db9c14
Demi Marie Obenour <demiobenour@gmail.com>
2025-03-25 14:58:32 -0400

    HTTP: Use common header validation function for HTTP/2 and HTTP/3
    The header validation required by HTTP/2 and HTTP/3 is identical, so use
    a common function for both.  This will make it easier to add additional
    validation in the future.  Move the function to ngx_http_parse.c so that
    it can share code with the HTTP/1.x parser in the future.
    
    No functional change intended.

--- src/http/ngx_http_parse.c
+++ src/http/ngx_http_parse.c
@@ -1127,6 +1127,68 @@ header_done:
 }
 
 
+#if (NGX_HTTP_V2 || NGX_HTTP_V3)
+ngx_int_t
+ngx_http_v23_validate_header(ngx_http_request_t *r, ngx_str_t *name,
+    ngx_str_t *value)
+{
+    u_char                     ch;
+    ngx_uint_t                 i;
+    ngx_http_core_srv_conf_t  *cscf;
+
+    r->invalid_header = 0;
+
+    cscf = ngx_http_get_module_srv_conf(r, ngx_http_core_module);
+
+    if (name->len < 1) {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                      "BUG: internal zero-length header name");
+
+        return NGX_ERROR;
+    }
+
+    for (i = (name->data[0] == ':'); i != name->len; i++) {
+        ch = name->data[i];
+
+        if ((ch >= 'a' && ch <= 'z')
+            || (ch == '-')
+            || (ch >= '0' && ch <= '9')
+            || (ch == '_' && cscf->underscores_in_headers))
+        {
+            continue;
+        }
+
+        if (ch <= 0x20 || ch == 0x7f || ch == ':'
+            || (ch >= 'A' && ch <= 'Z'))
+        {
+            ngx_log_error(NGX_LOG_INFO, r->connection->log, 0,
+                          "client sent invalid header name: \"%V\"",
+                          name);
+
+            return NGX_ERROR;
+        }
+
+        r->invalid_header = 1;
+    }
+
+    for (i = 0; i != value->len; i++) {
+        ch = value->data[i];
+
+        if (ch == '\0' || ch == LF || ch == CR) {
+            ngx_log_error(NGX_LOG_INFO, r->connection->log, 0,
+                          "client sent header \"%V\" with "
+                          "invalid value: \"%V\"",
+                          name, value);
+
+            return NGX_ERROR;
+        }
+    }
+
+    return NGX_OK;
+}
+#endif
+
+
 ngx_int_t
 ngx_http_parse_uri(ngx_http_request_t *r)
 {
