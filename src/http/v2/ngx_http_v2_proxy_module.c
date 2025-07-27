05f5c406641b2e78c09ed089034300034def50d6
Zhidao HONG <z.hong@f5.com>
2025-07-27 18:12:49 +0000

    HTTP/2: cache support.

--- src/http/v2/ngx_http_v2_proxy_module.c
+++ src/http/v2/ngx_http_v2_proxy_module.c
@@ -212,6 +212,9 @@ ngx_http_v2_proxy_handler(ngx_http_request_t *r)
     ngx_http_upstream_t        *u;
     ngx_http_v2_proxy_ctx_t    *ctx;
     ngx_http_proxy_loc_conf_t  *plcf;
+#if (NGX_HTTP_CACHE)
+    ngx_http_proxy_main_conf_t *pmcf;
+#endif
 
     if (ngx_http_upstream_create(r) != NGX_OK) {
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
@@ -250,6 +253,12 @@ ngx_http_v2_proxy_handler(ngx_http_request_t *r)
     u->output.tag = (ngx_buf_tag_t) &ngx_http_v2_proxy_module;
 
     u->conf = &plcf->upstream;
+
+#if (NGX_HTTP_CACHE)
+    pmcf = ngx_http_get_module_main_conf(r, ngx_http_proxy_module);
+    u->caches = &pmcf->caches;
+    u->create_key = ngx_http_proxy_create_key;
+#endif
 
     u->create_request = ngx_http_v2_proxy_create_request;
     u->reinit_request = ngx_http_v2_proxy_reinit_request;
@@ -306,10 +315,17 @@ ngx_http_v2_proxy_create_request(ngx_http_request_t *r)
     ngx_http_v2_proxy_frame_t    *f;
     ngx_http_proxy_loc_conf_t    *plcf;
     ngx_http_script_len_code_pt   lcode;
+    ngx_http_proxy_headers_t     *headers;
 
     u = r->upstream;
 
     plcf = ngx_http_get_module_loc_conf(r, ngx_http_proxy_module);
+
+#if (NGX_HTTP_CACHE)
+    headers = u->cacheable ? &plcf->headers_cache : &plcf->headers;
+#else
+    headers = &plcf->headers;
+#endif
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_v2_proxy_module);
 
@@ -367,7 +383,7 @@ ngx_http_v2_proxy_create_request(ngx_http_request_t *r)
     ngx_memzero(&le, sizeof(ngx_http_script_engine_t));
 
     ngx_http_script_flush_no_cacheable_variables(r, plcf->body_flushes);
-    ngx_http_script_flush_no_cacheable_variables(r, plcf->headers.flushes);
+    ngx_http_script_flush_no_cacheable_variables(r, headers->flushes);
 
     body_len = 0;
 
@@ -393,7 +409,7 @@ ngx_http_v2_proxy_create_request(ngx_http_request_t *r)
         ctx->ctx.internal_body_length = r->headers_in.content_length_n;
     }
 
-    le.ip = plcf->headers.lengths->elts;
+    le.ip = headers->lengths->elts;
     le.request = r;
     le.flushed = 1;
 
@@ -600,11 +616,11 @@ ngx_http_v2_proxy_create_request(ngx_http_request_t *r)
 
     ngx_memzero(&e, sizeof(ngx_http_script_engine_t));
 
-    e.ip = plcf->headers.values->elts;
+    e.ip = headers->values->elts;
     e.request = r;
     e.flushed = 1;
 
-    le.ip = plcf->headers.lengths->elts;
+    le.ip = headers->lengths->elts;
 
     while (*(uintptr_t *) le.ip) {
 
@@ -4378,6 +4394,10 @@ ngx_http_v2_proxy_get_connection_data(ngx_http_request_t *r,
 
     c = pc->connection;
 
+    if (c == NULL) {
+        goto create;
+    }
+
     if (pc->cached) {
 
         /*
@@ -4415,6 +4435,25 @@ ngx_http_v2_proxy_get_connection_data(ngx_http_request_t *r,
 
     cln->handler = ngx_http_v2_proxy_cleanup;
     ctx->connection = cln->data;
+
+    ctx->connection->init_window = NGX_HTTP_V2_DEFAULT_WINDOW;
+    ctx->connection->send_window = NGX_HTTP_V2_DEFAULT_WINDOW;
+    ctx->connection->recv_window = NGX_HTTP_V2_MAX_WINDOW;
+
+    ctx->send_window = NGX_HTTP_V2_DEFAULT_WINDOW;
+    ctx->recv_window = NGX_HTTP_V2_MAX_WINDOW;
+
+    ctx->id = 1;
+    ctx->connection->last_stream_id = 1;
+
+    return NGX_OK;
+
+create:
+
+    ctx->connection = ngx_pcalloc(r->pool, sizeof(ngx_http_v2_proxy_conn_t));
+    if (ctx->connection == NULL) {
+        return NGX_ERROR;
+    }
 
     ctx->connection->init_window = NGX_HTTP_V2_DEFAULT_WINDOW;
     ctx->connection->send_window = NGX_HTTP_V2_DEFAULT_WINDOW;
