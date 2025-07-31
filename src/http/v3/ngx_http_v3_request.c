ed50d0ee67c3b37b1c2c827c410299037fbcde6a
Sergey Kandaurov <pluknet@nginx.com>
2025-07-31 21:31:27 +0400

    Updated ngx_http_process_multi_header_lines() comments.
    Missed in fcf4331a0.

--- src/http/v3/ngx_http_v3_request.c
+++ src/http/v3/ngx_http_v3_request.c
@@ -1216,7 +1216,7 @@ ngx_http_v3_construct_cookie_header(ngx_http_request_t *r)
     if (hh->handler(r, h, hh->offset) != NGX_OK) {
         /*
          * request has been finalized already
-         * in ngx_http_process_multi_header_lines()
+         * in ngx_http_process_header_line()
          */
         return NGX_ERROR;
     }
