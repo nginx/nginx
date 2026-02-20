1a93eb7118822be51757fa5ee776fbfb4045229f
Elijah Zupancic <e.zupancic@f5.com>
2026-02-20 01:37:04 +0000

    HTTP: request body: clear body state on errors in HTTP/2 and HTTP/3.
    Previously, after errors the request body was left in a potentially
    inconsistent state in the HTTP/2 and HTTP/3 code paths, with
    r->headers_in.content_length_n which might be larger than buffers
    actually stored in r->request_body->bufs (or not set at all).  This
    can cause issues if the request body is subsequently used during
    error_page handling, such as when proxying.
    
    Fix is to clear r->request_body->bufs and set
    r->headers_in.content_length_n to 0 in HTTP/2 and HTTP/3 error
    paths, much like it is done in the HTTP/1.x code.
    
    Adaptation-Commit-For: https://freenginx.org/hg/nginx/rev/81082b5521ddb55a55028468544d30283bc198d7

--- src/http/v2/ngx_http_v2.c
+++ src/http/v2/ngx_http_v2.c
@@ -1112,7 +1112,11 @@ ngx_http_v2_state_read_data(ngx_http_v2_connection_t *h2c, u_char *pos,
                                               stream->in_closed, 0);
 
         if (rc != NGX_OK && rc != NGX_AGAIN) {
+
             stream->skip_data = 1;
+            r->headers_in.content_length_n = 0;
+            r->request_body->bufs = NULL;
+
             ngx_http_finalize_request(r, rc);
         }
 
@@ -3862,6 +3866,7 @@ ngx_http_v2_run_request(ngx_http_request_t *r)
                           "client prematurely closed stream");
 
             r->stream->skip_data = 1;
+            r->headers_in.content_length_n = 0;
 
             ngx_http_finalize_request(r, NGX_HTTP_BAD_REQUEST);
             goto failed;
@@ -4303,6 +4308,8 @@ ngx_http_v2_read_client_request_body_handler(ngx_http_request_t *r)
 
     if (rc != NGX_OK && rc != NGX_AGAIN) {
         r->stream->skip_data = 1;
+        r->headers_in.content_length_n = 0;
+        r->request_body->bufs = NULL;
         ngx_http_finalize_request(r, rc);
         return;
     }
@@ -4343,6 +4350,8 @@ ngx_http_v2_read_client_request_body_handler(ngx_http_request_t *r)
                           "http2 negative window update");
 
             stream->skip_data = 1;
+            r->headers_in.content_length_n = 0;
+            r->request_body->bufs = NULL;
 
             ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
             return;
@@ -4356,6 +4365,8 @@ ngx_http_v2_read_client_request_body_handler(ngx_http_request_t *r)
         == NGX_ERROR)
     {
         stream->skip_data = 1;
+        r->headers_in.content_length_n = 0;
+        r->request_body->bufs = NULL;
         ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
         return;
     }
@@ -4364,6 +4375,8 @@ ngx_http_v2_read_client_request_body_handler(ngx_http_request_t *r)
 
     if (ngx_http_v2_send_output_queue(h2c) == NGX_ERROR) {
         stream->skip_data = 1;
+        r->headers_in.content_length_n = 0;
+        r->request_body->bufs = NULL;
         ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
         return;
     }
