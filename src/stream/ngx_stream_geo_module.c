0de6e878ba43b55dd23b437c5be1819a55f63ec4
Eugene Grebenschikov <e.grebenshchikov@f5.com>
2026-03-11 17:57:05 -0700

    Fixed the "include" directive inside the "geo" block.
    The "include" directive should be able to include multiple files if
    given a filename mask.
    
    Completes remaining changes introduced in da4ffd8.
    
    Closes: https://github.com/nginx/nginx/issues/1165

--- src/stream/ngx_stream_geo_module.c
+++ src/stream/ngx_stream_geo_module.c
@@ -605,7 +605,12 @@ ngx_stream_geo(ngx_conf_t *cf, ngx_command_t *dummy, void *conf)
 
     if (ngx_strcmp(value[0].data, "include") == 0) {
 
-        rv = ngx_stream_geo_include(cf, ctx, &value[1]);
+        if (strpbrk((char *) value[1].data, "*?[") == NULL) {
+            rv = ngx_stream_geo_include(cf, ctx, &value[1]);
+
+        } else {
+            rv = ngx_conf_include(cf, dummy, conf);
+        }
 
         goto done;
     }
