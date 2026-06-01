4939a63dcbd6abd156389c4290a21341ac8dc0b7
Zhidao HONG <z.hong@f5.com>
2026-06-01 08:41:33 +0000

    Proxy: fix HTTP/2 INITIAL_WINDOW_SIZE saved state
    The window size change was previously kept in a local variable while
    processing SETTINGS parameters.  If parsing was suspended after
    SETTINGS_INITIAL_WINDOW_SIZE, the value was lost on resume and was
    acknowledged without being applied.
    
    Store the parsed initial window size in the HTTP/2 state until the
    SETTINGS frame is fully processed.

--- src/http/v2/ngx_http_v2.h
+++ src/http/v2/ngx_http_v2.h
@@ -80,10 +80,12 @@ typedef struct {
     ngx_uint_t                       sid;
     size_t                           length;
     size_t                           padding;
+    size_t                           init_window_size;
     unsigned                         flags:8;
 
     unsigned                         incomplete:1;
     unsigned                         keep_pool:1;
+    unsigned                         init_window_set:1;
 
     /* HPACK */
     unsigned                         parse_name:1;
