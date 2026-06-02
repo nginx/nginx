25273ede01969405c27535ae80644abaf71da443
Eugene Grebenschikov <e.grebenshchikov@f5.com>
2026-06-02 14:39:36 -0700

    Version bump

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1031001
-#define NGINX_VERSION      "1.31.1"
+#define nginx_version      1031002
+#define NGINX_VERSION      "1.31.2"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
