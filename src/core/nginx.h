d01f110b817a8a2092114579965aefb510993762
Vadim Zhestikov <v.zhestikov@f5.com>
2026-07-16 12:21:48 -0700

    Version bump

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1031003
-#define NGINX_VERSION      "1.31.3"
+#define nginx_version      1031004
+#define NGINX_VERSION      "1.31.4"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
