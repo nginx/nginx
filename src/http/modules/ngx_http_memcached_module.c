ba02005513657dc5880f87b6b7a6cba341e89dd0
Vinay Kumar Tokala <v.tokala@f5.com>
2026-09-07 05:57:21 -0700

    Upstream: proxy_header_timeout and friends
    proxy_header_timeout and friends directive defines a timeout for
    receiving the response header from the proxied server
    The timeout is measured from the moment the connection to the server
    is initiated until the response headers are fully received
    Unlike proxy_read_timeout, which is reset on each successful read,
    this timeout is a wall-clock deadline that is not extended by
    activity on the connection.
    If the full response header has not been received,
    before the timeout expires, the attempt is treated as failed and
    subject to the timeout.

--- src/http/modules/ngx_http_memcached_module.c
+++ src/http/modules/ngx_http_memcached_module.c
@@ -102,6 +102,13 @@ static ngx_command_t  ngx_http_memcached_commands[] = {
       offsetof(ngx_http_memcached_loc_conf_t, upstream.read_timeout),
       NULL },
 
+    { ngx_string("memcached_header_timeout"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_msec_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_memcached_loc_conf_t, upstream.header_timeout),
+      NULL },
+
     { ngx_string("memcached_next_upstream"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
       ngx_conf_set_bitmask_slot,
@@ -615,6 +622,7 @@ ngx_http_memcached_create_loc_conf(ngx_conf_t *cf)
     conf->upstream.connect_timeout = NGX_CONF_UNSET_MSEC;
     conf->upstream.send_timeout = NGX_CONF_UNSET_MSEC;
     conf->upstream.read_timeout = NGX_CONF_UNSET_MSEC;
+    conf->upstream.header_timeout = NGX_CONF_UNSET_MSEC;
     conf->upstream.next_upstream_timeout = NGX_CONF_UNSET_MSEC;
 
     conf->upstream.buffer_size = NGX_CONF_UNSET_SIZE;
@@ -664,6 +672,9 @@ ngx_http_memcached_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 
     ngx_conf_merge_msec_value(conf->upstream.read_timeout,
                               prev->upstream.read_timeout, 60000);
+
+    ngx_conf_merge_msec_value(conf->upstream.header_timeout,
+                              prev->upstream.header_timeout, 0);
 
     ngx_conf_merge_msec_value(conf->upstream.next_upstream_timeout,
                               prev->upstream.next_upstream_timeout, 0);
